37c4a975f2330c6d837d8558695b9da568c56be3
Bellotiny <donissaint13@gmail.com>
2024-11-01 13:13:45 -0400

    Added verifyRights()
    Added basic verifyRights() in Controller

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -7,4 +7,15 @@ class Controller {
         extract($data);
         include "Views/$controller/$view.php";
     }
+
+    function verifyRights($id, $contoller ='user', $action='list'){
+        $sql = "select COUNT(users.id) users
+                inner join users_groups on users.id = users_groups.userID
+                inner join groups_rights on users_groups.id = groups_rights.groupID
+                inner join rights on groups_rights.rightsID = rights.rightsID
+
+                where users.id = 1
+                AND rights.action LIKE 'delete'
+                AND rights.controller LIKE 'employee'";
+    }
 }
