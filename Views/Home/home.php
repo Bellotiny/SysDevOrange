5d2c09a03408a92c8607b5ced961c743930bdad5
Maxime Mir-Orefice <maxime.mirorefice@gmail.com>
2024-11-03 20:52:41 -0500

    Add Janna's changes

--- Views/Home/home.php
+++ Views/Home/home.php
@@ -26,12 +26,12 @@ include_once 'Views/nav.php';
           <!-- Login Form (Initially Hidden) -->
           <form id="loginForm" method="POST" action="<?=BASE_PATH?>/account/login" style="display: none;">
             <div class="form-group">
-              <label for="loginEmail">Email</label>
-              <input type="email" class="form-control" id="loginEmail" name="email" required>
+              <label for="email">Email</label>
+              <input type="email" class="form-control" id="email" name="email" required>
             </div>
             <div class="form-group">
-              <label for="loginPassword">Password</label>
-              <input type="password" class="form-control" id="loginPassword" name="password" required>
+              <label for="password">Password</label>
+              <input type="password" class="form-control" id="password" name="password" required>
             </div>
             <button type="submit" class="btn btn-primary mt-3">Login</button>
             <button type="button" class="btn btn-secondary mt-3" id="goBackFromLogin">Go Back</button>
@@ -40,21 +40,25 @@ include_once 'Views/nav.php';
 
           <!-- Register Form (Initially Hidden) -->
           <form id="registerForm" method="POST" action="<?=BASE_PATH?>/account/register" style="display: none;">
-            <div class="form-group">
-              <label for="fname">First Name</label>
-              <input type="text" class="form-control" id="fname" name="fname" required>
+            <div class="form-group py-2">
+                <label for="firstName">First Name</label>
+                <input type="text" class="form-control" id="firstName" name="firstName" required>
             </div>
-            <div class="form-group">
-              <label for="lname">Last Name</label>
-              <input type="text" class="form-control" id="lname" name="lname" required>
+            <div class="form-group py-2">
+                <label for="lastName">Last Name</label>
+                <input type="text" class="form-control" id="lastName" name="lastName" required>
             </div>
-            <div class="form-group">
-              <label for="registerEmail">Email</label>
-              <input type="email" class="form-control" id="registerEmail" name="email" required>
+            <div class="form-group py-2">
+                <label for="birthDate">Birthday:</label>
+                <input type="date" class="form-control"  id="birthDate" name="birthDate" >
             </div>
-            <div class="form-group">
-              <label for="registerPassword">Password</label>
-              <input type="password" class="form-control" id="registerPassword" name="password" required>
+            <div class="form-group py-2">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+            </div>
+            <div class="form-group py-2">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password" required>
             </div>
             <button type="submit" class="btn btn-primary mt-3">Register</button>
             <button type="button" class="btn btn-secondary mt-3" id="goBackFromRegister">Go Back</button>
