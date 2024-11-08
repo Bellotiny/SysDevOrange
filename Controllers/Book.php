7311c167e87aa912d3f9686eb27ee4d4ec9e6f28
Bellotiny <donissaint13@gmail.com>
2024-11-08 14:25:43 -0500

    Starting Book controller
    Only created the actions switch

--- Controllers/Book.php
+++ Controllers/Book.php
@@ -1,6 +1,11 @@
 <?php
 
 include_once "Controllers/Controller.php";
+include_once "Models/Booking.php";
+include_once "Models/Service.php";
+include_once "Models/Discount.php";
+include_once "Models/Payment.php";
+include_once "Models/User.php";
 
 class Book extends Controller {
     public static function redirect(string $action = ""): void {
@@ -9,6 +14,50 @@ class Book extends Controller {
 
     public function route(): void {
         $action = strtolower($_GET['action']) ?? "bookOne";
+
+        switch ($action) {
+            case "list":
+                
+            break;
+            case "add":
+                $data = isset($_POST[]);
+                $services = Service::list();
+                $colors = Color::list();
+                $user = User::add();
+                $this->render("Book", "book", ["services" => $services, "colors" => $colors]);
+            break;
+            case "delete":
+                if($this->verifyRights($action)){
+
+                }
+            break;
+            case "view":
+                if($this->verifyRights($action)){
+                    
+                }
+            break;
+            case "edit":
+                if($this->verifyRights($action)){
+
+                }
+            break;
+            case "deleteOthers":
+                if($this->verifyRights($action)){
+
+                }
+            break;
+            case "viewOthers":
+                if($this->verifyRights($action)){
+
+                }
+            break;
+            case "editOthers":
+                if($this->verifyRights($action)){
+
+                }
+            break;
+                
+        }
 
         $this->render("Book", $action);
     }
