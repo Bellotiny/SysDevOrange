eaafb7cfbdd20dd4c8e1ae6c7012308f77e93963
Bellotiny <donissaint13@gmail.com>
2024-11-20 10:47:33 -0500

    Revert "Add action in book"
    This reverts commit 255af09b3c45909b059e40378617003cda5d94d4.

--- Models/Availability.php
+++ Models/Availability.php
@@ -3,13 +3,13 @@
 include_once "Model.php";
 
 class Availability extends Model {
-    public int $timeslot;
-    public int $bookingID;
+    public int $start;
+    public int $end;
 
     public function __construct(array $fields) {
         $this->id = $fields[self::getTable() . '.id'];
-        $this->timeslot = $fields[self::getTable() . '.timeslot'];
-        $this->bookingID = $fields[self::getTable() . '.bookingID'];
+        $this->start = $fields[self::getTable() . '.start'];
+        $this->end = $fields[self::getTable() . '.end'];
     }
 
     public static function getTable(): string {
@@ -17,21 +17,21 @@ class Availability extends Model {
     }
 
     public static function getFields(): array {
-        return ["id", "timeslot", "bookingID"];
+        return ["id", "start", "end"];
     }
 
-    public static function new(int $timeslot, int $end): ?Availability {
+    public static function new(int $start, int $end): ?Availability {
         $values = new Values();
-        $values->add(new Value(self::getTable() . ".timeslot", $start));
-        $values->add(new Value(self::getTable() . ".bookingID", $end));
+        $values->add(new Value(self::getTable() . ".start", $start));
+        $values->add(new Value(self::getTable() . ".end", $end));
 
         try {
             self::insert($values, false);
             $id = self::getConnection()->insert_id;
             return new Availability([
                 self::getTable() . ".id" => $id,
-                self::getTable() . ".timeslot" => $start,
-                self::getTable() . ".bookingID" => $end,
+                self::getTable() . ".start" => $start,
+                self::getTable() . ".end" => $end,
             ]);
         } catch (Exception) {
             return null;
@@ -40,8 +40,8 @@ class Availability extends Model {
 
     public function save(): bool {
         $values = new Values();
-        $values->add(new Value(self::getTable() . ".timeslot", $this->timeslot));
-        $values->add(new Value(self::getTable() . ".bookingID", $this->bookingID));
+        $values->add(new Value(self::getTable() . ".start", $this->start));
+        $values->add(new Value(self::getTable() . ".end", $this->end));
         $where = new Where();
         $where->addEquals(new Value(self::getTable() . ".id", $this->id));
         return self::update($values, $where);
