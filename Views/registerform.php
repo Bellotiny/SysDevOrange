80c4c2aa983af9ab46536fa1e25701c7db940a7c
Trevor Obodoechina <75284196+TrevCODE@users.noreply.github.com>
2024-11-21 15:48:50 -0500

    Added french and english (not done)
    - Added localization to: Home, register form
    - Moved Cookies  to index.php

--- Views/registerform.php
+++ Views/registerform.php
@@ -3,43 +3,43 @@
          <form method="POST" action="<?=BASE_PATH?>/account/register" >
           
             <div class="form-group py-2">
-              <label for="firstName">First Name</label>
+              <label for="firstName"><?= FIRST_NAME ?></label>
               <input type="text" class="form-control" id="firstName" name="firstName" value="<?= $data["firstName"] ?? "" ?>" required>
             </div>
             <div class="form-group py-2">
-              <label for="lastName">Last Name</label>
+              <label for="lastName"><?= LAST_NAME ?></label>
               <input type="text" class="form-control" id="lastName" name="lastName" value="<?= $data["lastName"] ?? "" ?>" required>
             </div>
              <div class="form-group py-2">
-                 <label for="email">Email</label>
+                 <label for="email"><?= EMAIL ?></label>
                  <input type="email" class="form-control" id="email" name="email" value="<?= $data["email"] ?? "" ?>" required>
              </div>
             <div class="form-group py-2">
-              <label for="birthDate">Birthday:</label>
+              <label for="birthDate"><?= BIRTHDAY ?></label>
               <input type="date" class="form-control"  id="birthDate" name="birthDate" value="<?= $data["birthDate"] ?? "" ?>">
             </div>
             <div class="form-group py-2">
-              <label for="phoneNumber">Phone Number</label>
+              <label for="phoneNumber"><?= PHONE_NUMBER ?></label>
               <input type="tel" class="form-control" id="phoneNumber" name="phoneNumber" value="<?= $data["phoneNumber"] ?? "" ?>">
             </div>
             <div class="form-group py-2">
-              <label for="password">Password</label>
+              <label for="password"><?= PASSWORD ?></label>
               <input type="password" class="form-control" id="password" name="password" required>
             </div>
             <div class="form-group py-2">
-              <label for="confirmPassword">Confirm Password</label>
+              <label for="confirmPassword"><?= CONFIRM_PASSWORD ?></label>
               <input type="password" class="form-control" id="confirmPassword" name="confirmPassword" required>
             </div>
             <?php if (isset($data["error"])): ?>
              <p><span style="color:red"><?php echo $data['error']; ?></span></p>
             <?php endif; ?>
             <ul>
-            Password must contain
-              <li>1 uppercase</li>
-              <li>1 lowercase</li>
-              <li>1 number</li>
-              <li>1 symbol</li>
-              <li>Minimum of 6 characters</li>
+            <?= PASS_MUST_CONTAIN ?>
+              <li>1 <?= UPPERCASE ?></li>
+              <li>1 <?= LOWERCASE ?></li>
+              <li>1 <?= NUMBER ?></li>
+              <li>1 <?= SYMBOL ?></li>
+              <li><?= MIN_SIX_CHAR ?></li>
             </ul>
             
             
