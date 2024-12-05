7eeee793018cebb72f87dc9f53346504adfc526b
Trevor Obodoechina <75284196+TrevCODE@users.noreply.github.com>
2024-12-05 12:05:11 -0500

    small update

--- Views/Account/history.php
+++ Views/Account/history.php
@@ -16,7 +16,8 @@ include_once 'Views/bookingModal.php';
     <div class="sideScreen slide-up">
         <!---default content will be the Personal info-->
         <!--content is load here-->
-        <div class="w-100 slide-up">
+        <div class="w-100 slide-up container  my-2">
+        <?php include_once 'Views/menuAccount.php'; ?>
             <div class="container mx-3">
                 <div class="row">
             
